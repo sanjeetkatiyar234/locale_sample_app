1de7c62d97705f59dfba3fadf98fec8ba69a4fc0
sanjeet katiyar <sanjeet.katiyar234@gmail.com>
2022-11-01 17:28:33 +0530

    Make alpha color value functioning

--- src/utils/convertRgbToArray.js
+++ src/utils/convertRgbToArray.js
@@ -1,5 +1,11 @@
 
 export const convertRbgtoArray=(rgbValue)=>{
     if(rgbValue)
-    return rgbValue?.replace('(', ',').replace(')', '').split(",").slice(1).map(v=>+v);
+    return rgbValue?.replace('(', ',').replace(')', '').split(",").slice(1).map((value,index)=>{
+        if(index===3)
+       return parseInt(+value * 255); 
+       else
+        return +value;
+    }
+    );
 }
