8d09392d33a98820b33ecf0808d221438c639f33
Sanjeet Katiyar <sanjeet.katiyar234@gmail.com>
2022-11-23 23:31:12 +0530

    Add user avatar user name and logout option

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,10 +1,19 @@
 import MenuIcon from "@mui/icons-material/Menu";
-import { IconButton, Toolbar, Typography } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import MuiAppBar from "@mui/material/AppBar";
 import { styled } from "@mui/material/styles";
 import React from "react";
 import smallLogo from "./smallLogo.png";
 import { drawerWidth } from "./SideBar";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { AccountCircle } from "@mui/icons-material";
 
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== "open",
@@ -25,33 +34,92 @@ const AppBar = styled(MuiAppBar, {
 }));
 
 const Header = ({ open, handleDrawerOpen }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <AppBar
       position="fixed"
       open={open}
       style={{ backgroundColor: "rgb(42, 42, 42)" }}
     >
-      <Toolbar style={{ paddingLeft: "10px" }}>
-        {!open && (
-          <img
-            src={smallLogo}
-            alt="fireSpot"
-            height="40"
-            style={{ marginRight: "5px" }}
-          />
-        )}
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          onClick={handleDrawerOpen}
-          edge="start"
-          sx={{
-            marginRight: 5,
-            ...(open && { display: "none" }),
-          }}
-        >
-          <MenuIcon />
-        </IconButton>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          paddingLeft: "10px",
+        }}
+      >
+        <div>
+          {!open && (
+            <img
+              src={smallLogo}
+              alt="fireSpot"
+              height="40"
+              style={{ marginRight: "5px" }}
+            />
+          )}
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: "none" }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </div>
+        <div>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <AccountCircle />
+            <Typography variant="body1" sx={{ m: 1 }}>
+              Test User
+            </Typography>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <KeyboardArrowDownIcon />
+            </IconButton>
+          </Box>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
+        </div>
       </Toolbar>
     </AppBar>
   );
