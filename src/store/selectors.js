8fb8fec3264b2f35a049a143093537d2870f9d18
Sanjeet Katiyar <sanjeet.katiyar234@gmail.com>
2022-12-02 20:51:16 +0530

    remove ui filtering for daily of origin-destination-1

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,22 +1,12 @@
-import moment from "moment";
 import { createSelector } from "reselect";
 
 // state selectors
 const queryHiveDataSelector = (state) => state.data.queryHive;
-const filterTypeValueSelector = (state) => state.filterType.value;
 
 export const queryHiveDataWithFilterTypeSelector = createSelector(
-  [queryHiveDataSelector, filterTypeValueSelector],
-  (sampleData = [], filterTypeValue) => {
-    const todayDate = moment().subtract(10, "M");
-    const filterData =
-      filterTypeValue === "daily"
-        ? sampleData.filter((d) =>
-            moment(d.start_time).isSameOrAfter(todayDate)
-          )
-        : sampleData;
-
-    return filterData?.filter(
+  [queryHiveDataSelector],
+  (sampleData) => {
+    return sampleData?.filter(
       (d) =>
         d.start_loc[0] != null &&
         d.start_loc[1] != null &&
