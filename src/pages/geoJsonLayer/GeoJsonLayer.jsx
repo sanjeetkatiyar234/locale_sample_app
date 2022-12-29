4b8cab6550d417fe1869a667ae5bbae22adce984
Sanjeet Katiyar <sanjeet.katiyar234@gmail.com>
2022-12-29 10:13:29 +0530

    uncomments console

--- src/pages/geoJsonLayer/GeoJsonLayer.jsx
+++ src/pages/geoJsonLayer/GeoJsonLayer.jsx
@@ -21,8 +21,8 @@ const GeoJsonLayer = () => {
     dateRange
   );
   // console.log("firstGeodata", firstGeodata);
-  // console.log("GeoJsondata", GeoJsondata.features);
-  // console.log("filterData", filterData);
+  console.log("GeoJsondata", GeoJsondata.features);
+  console.log("filterData", filterData);
   return (
     <div className="geo-json-layer">
       <PageHeader />
